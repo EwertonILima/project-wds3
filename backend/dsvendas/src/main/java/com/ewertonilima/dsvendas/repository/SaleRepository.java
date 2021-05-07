7e8ff7543ae30fa46f1b9bead20239005d4f631d
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-07 16:47:06 -0300

    Group by search

--- backend/dsvendas/src/main/java/com/ewertonilima/dsvendas/repository/SaleRepository.java
+++ backend/dsvendas/src/main/java/com/ewertonilima/dsvendas/repository/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.ewertonilima.dsvendas.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ewertonilima.dsvendas.dto.SaleSuccessDTO;
+import com.ewertonilima.dsvendas.dto.SaleSumDTO;
 import com.ewertonilima.dsvendas.entities.Sale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long>{
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.ewertonilima.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+
+	@Query("SELECT new com.ewertonilima.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
